1905d87ef88497804138092324ffce8fa0bfb7f6
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-25 12:34:53 +0530

    uc5: To check password is valid or not

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -45,3 +45,29 @@ valid_mobile_number()
         fi
 }
 valid_mobile_number $mobile_number
+read -p " enter password  : " password
+valid_password()
+{
+        if [[ $1 =~ ^([a-zA-Z0-9@#!]){8,}$ ]]
+        then
+                if [[ $1 =~ ^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$ ]]
+                then
+                        if [[ $1 =~ ^[a-zA-Z@#!]*[0-9]+[a-zA-Z@#!]*$ ]]
+                        then
+                                if [[ $1 =~ ^([a-zA-Z0-9]*)[^a-zA-Z_0-9\s]([a-zA-Z0-9]*)$ ]]
+                                then
+                                        echo "valid password"
+                                else
+                                        echo "atleast one special character required in password"
+                                fi
+                        else
+                                echo "atleast one numeric number is required in password"
+                        fi
+                else
+                        echo "password must have atleast one capital"
+                fi
+        else
+                echo "password sholud contain minimum 8 characters"                                                             fi
+	fi
+}
+valid_password $password
