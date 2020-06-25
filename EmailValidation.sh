f0256f73a20d775bb544bbf73d26ed42d82d7aec
revathi chandana <chandanarevathi30@gmail.com>
2020-06-25 11:33:45 +0530

    uc1: To check first name is valid or not

--- EmailValidation.sh
+++ EmailValidation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 shopt -s extglob
 echo "To check email validation"
+read -p " enter a valid first name : " first_name
+print_valid_First_name()
+{
+        if [[ $1 =~ ^([A-Z]{1}+[a-z0-9]{3,})$ ]]
+        then
+                echo "first name is validate."
+        else
+                echo "first name is not validate."
+        fi
+}
+print_valid_First_name $first_name
